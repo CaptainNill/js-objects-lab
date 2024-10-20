6fb0eeee44bd91b7d8759b1a5bfdd13b4c1d2aef
Captain Nill <nillpaeez1997@gmail.com>
2024-10-20 14:12:45 -0500

    save

--- app.js
+++ app.js
@@ -229,7 +229,11 @@ This method should:
 
 Solve Exercise 14 here:
 */
+game.partyCount = function(){
+    return game.party.length;
+}
 
+console.log(`The party count is: ${game.partyCount()}`);
 /*
 Exercise 15
 1. Now, complete gyms with a difficulty below 8. Reflect on how this is similar to or different from the previous gym exercises.
